ce7daac945ee0458ab224367941f499cd2ba38c9
kwonding <wldo8541@naver.com>
2025-09-08 09:14:24 +0900

    update for the latest

--- src/main/java/org/example/z_project/phr_solution/service/HealthRecordService.java
+++ src/main/java/org/example/z_project/phr_solution/service/HealthRecordService.java
@@ -6,9 +6,13 @@ import org.example.z_project.phr_solution.dto.health_record.response.RecordListR
 import java.util.List;
 
 public interface HealthRecordService {
+    // 생성
     void createRecord(RecordCreateRequestDto dto);
+    // 전체 조회
     List<RecordListResponseDto> getAllRecords();
+    // 단건 조회
     List<RecordListResponseDto> filterRecordsByDiagnosis(String diagnosis);
+    // 삭제
 //    void deleteRecord(Long id);
     boolean deleteRecord(Long id);
 }
